1de91cb13b29b0e17411c1a2102a26d4cb534faa
mamthakumari36 <144232603+mamthakumari36@users.noreply.github.com>
2023-09-06 23:24:12 +0530

    Add files via upload

---
+++ aboutus.vb
@@ -0,0 +1,10 @@
+﻿Public Class aboutus
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Me.Close()
+        options.Show()
+    End Sub
+
+    Private Sub aboutus_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
